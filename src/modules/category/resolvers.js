633cb74980fe9ea94483bb64b56f64396c858320
Otis Chiemezie <otisidevelop@gmail.com>
2020-12-12 13:58:27 +0100

    Added category module.

---
+++ src/modules/category/resolvers.js
@@ -0,0 +1,49 @@
+const { AuthenticationError } = require("apollo-server");
+const { CategoryService } = require("../../service/category.service");
+
+const resolvers = {
+    Query: {
+        GetCategory: async (_, { id }, { user }) => {
+            if (user && user.isAdmin) {
+                const res = await CategoryService.GetCategory(id);
+                return res;
+            }
+            return new AuthenticationError("Unauthorized access!");
+        },
+        GetCategories: async (_, __, { user }) => {
+            if (user && user.isAdmin) {
+                const res = await CategoryService.GetCategories();
+                return res;
+            }
+            return new AuthenticationError("Unauthorized access!");
+        }
+    },
+    Mutation: {
+        NewCategory: async (_, { model }, { user }) => {
+            if (user && user.isAdmin) {
+                const result = await CategoryService.CreateCategory(model);
+                return result;
+            }
+            return new AuthenticationError("Unauthorized access!");
+        },
+        UpdateCategory: async (_, { id, model }, { user }) => {
+            if (user && user.isAdmin) {
+                const res = await CategoryService.UpdateCategory(id, model);
+                return res;
+            }
+            return new AuthenticationError("Unauthorized access!");
+        },
+        DeleteCategory: async (_, { id }, { user }) => {
+            if (user && user.isAdmin) {
+                const res = await CategoryService.DeleteCategory(id);
+                return res;
+            }
+            return new AuthenticationError("Unauthorized access!");
+        }
+    },
+    Category: {
+        created_at: ({ created_at }) => new Date(created_at).toISOString()
+    }
+};
+
+exports.resolvers = resolvers;
---
+++ src/modules/category/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+    extend type Query {
+        GetCategory(id: ID!): CategoryResponse!
+        GetCategories: CategoriesResponse!
+    }
+    extend type Mutation {
+        NewCategory("Category input template" model: CategoryInput!): CategoryResponse!
+        UpdateCategory("Category id" id: ID!, model: CategoryInput!): CategoryResponse!
+        DeleteCategory("Category id" id: ID!): DeletedResponse!
+    }
+
+    "Category input template"
+    input CategoryInput {
+        "Title"
+        title: String!
+        "Description"
+        desc: String!
+    }
+
+    type CategoryResponse {
+        status: Int!
+        message: String!
+        doc: Category!
+    }
+
+    type CategoriesResponse {
+        status: Int!
+        message: String!
+        docs: [Category]!
+    }
+
+    "Category Template"
+    type Category {
+        id: ID!
+        "Category title"
+        title: String!
+        "Category description"
+        desc: String!
+        "Date of creation"
+        created_at: String!
+    }
+`;
+
+exports.typeDefs = typeDefs;
